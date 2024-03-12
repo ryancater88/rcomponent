d9327a18abe15e34d2552031e7f664bccc81876c
Seu Nome <mosico1442@fkcod.com>
2024-03-12 11:55:37 -0300

    Utilizando de classes para os componentes

--- script/main.js
+++ script/main.js
@@ -1,59 +1,88 @@
-var listaDeModaisAbertos = [];
 
-
-function rModalFechar(idModal){
- document.querySelector(`.rmodal-overlay[id="${idModal}"]`).remove();
-}
-
-function rModalFechar__Atalho(){
-	if(listaDeModaisAbertos != ''){
-		document.querySelector(`.rmodal-overlay[id="${listaDeModaisAbertos[listaDeModaisAbertos.length - 1]}"]`).remove();
-		listaDeModaisAbertos.pop()
+class Rmodal{
+	constructor(titulo, corpo, botaoSalvar, idModal){
+		this.titulo = titulo,
+		this.corpo = corpo,
+		this.botaoSalvar = botaoSalvar,
+		this.idModal = idModal
 	}
-}
+
+	 fechar(idModal = this.idModal){
+	document.querySelector(`.rmodal-overlay[id="${idModal}"]`).remove();
+	}
 	
+	
+	 abrir(title = this.titulo, bodycontent = this.corpo, havePrimaryButton = this.botaoSalvar){
+		const page = document.querySelector('body');
+		var minimo = 1;
+		var maximo = 1000;
+		var numeroIntervalo = Math.random() * (maximo - minimo) + minimo;
 
-function rModalAbrir(title, bodycontent, havePrimaryButton){
-    const page = document.querySelector('body');
-    var minimo = 1;
-    var maximo = 1000;
-    var numeroIntervalo = Math.random() * (maximo - minimo) + minimo;
-
-    page.insertAdjacentHTML('beforeend', 
-        `
-        <div class="rmodal-overlay" id="${numeroIntervalo}">
-		<div class="rmodal" id="${numeroIntervalo}">
-			<div class="rmodal-container" id="${numeroIntervalo}">
-				<div class="rmodal-header" id="${numeroIntervalo}">
-					<span class="rmodal-title" id="${numeroIntervalo}">${title}</span>
-					<div class="rmodal-closeBut"id="${numeroIntervalo}">
-						<button class="rmodal-closebutton" id="${numeroIntervalo}">X</button>
+		page.insertAdjacentHTML('beforeend', 
+			`
+			<div class="rmodal-overlay" id="${numeroIntervalo}">
+			<div class="rmodal" id="${numeroIntervalo}">
+				<div class="rmodal-container" id="${numeroIntervalo}">
+					<div class="rmodal-header" id="${numeroIntervalo}">
+						<span class="rmodal-title" id="${numeroIntervalo}">${title}</span>
+						<div class="rmodal-closeBut"id="${numeroIntervalo}">
+							<button class="rmodal-closebutton" id="${numeroIntervalo}">X</button>
+						</div>
 					</div>
-				</div>
-				<div class="rmodal-body" id="${numeroIntervalo}">
-					${bodycontent}
-				</div>
-				<div class="rmodal-footer" id="${numeroIntervalo}">
-					<div class="rmodal-groupbutton" id="${numeroIntervalo}">
-						<button class="rmodal-primarybutton" id="${numeroIntervalo}">Salvar</button>
-						<button class="rmodal-secondarybutton" id="${numeroIntervalo}">Fechar</button>
+					<div class="rmodal-body" id="${numeroIntervalo}">
+						${bodycontent}
+					</div>
+					<div class="rmodal-footer" id="${numeroIntervalo}">
+						<div class="rmodal-groupbutton" id="${numeroIntervalo}">
+							<button class="rmodal-primarybutton" id="${numeroIntervalo}">Salvar</button>
+							<button class="rmodal-secondarybutton" id="${numeroIntervalo}">Fechar</button>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-        `
-    )
+			`
+		)
 
-    const buttonClose = document.querySelector(`.rmodal-closebutton[id="${numeroIntervalo}"]`);
-	const buttonPrimary = document.querySelector(`.rmodal-primarybutton[id="${numeroIntervalo}"]`);
-    const buttonSecondary = document.querySelector(`.rmodal-secondarybutton[id="${numeroIntervalo}"]`);
-	listaDeModaisAbertos.push(numeroIntervalo);
+		const buttonClose = document.querySelector(`.rmodal-closebutton[id="${numeroIntervalo}"]`);
+		const buttonPrimary = document.querySelector(`.rmodal-primarybutton[id="${numeroIntervalo}"]`);
+		const buttonSecondary = document.querySelector(`.rmodal-secondarybutton[id="${numeroIntervalo}"]`);
 
-	if(!havePrimaryButton){
-		buttonPrimary.remove();
+		if(!havePrimaryButton){
+			buttonPrimary.remove();
+		}
+
+		buttonClose.addEventListener('click', () => {this.fechar(numeroIntervalo)});
+		buttonSecondary.addEventListener('click', () => {this.fechar(numeroIntervalo)});
+	};
+}
+
+class Rloader{
+	constructor(){
+		this.page = document.querySelector('body')
 	}
 
-    buttonClose.addEventListener('click', () => {rModalFechar(numeroIntervalo)});
-    buttonSecondary.addEventListener('click', () => {rModalFechar(numeroIntervalo)});
-};
+	mostrar(){
+		const loader = document.querySelector('.rloader-backdrop');
+
+		if(!loader){
+			const element = `
+			<div class="rloader-backdrop">
+			<div class="rloader-grouploader">
+				<div class="rloader-loader"></div>
+				<p>Carregando...</p> 
+			</div>
+		</div>`
+
+		this.page.insertAdjacentHTML('beforeend', element)
+		}
+	}
+
+	ocultar(){
+		const loader = document.querySelector('.rloader-backdrop');
+		
+		if(loader){
+			loader.remove()
+		}
+	}
+}
